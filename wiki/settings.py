2c9ad42f9f8039ca3b7215c50bd7012056c7dff3
Manish Lamichhane <manishlamichhane@gmail.com>
2019-06-10 15:54:12 +0200

    Fix template issues

--- wiki/settings.py
+++ wiki/settings.py
@@ -66,13 +66,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'wiki.urls'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(STATIC_ROOT, 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,6 +135,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
